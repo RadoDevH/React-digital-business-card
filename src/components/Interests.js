0b2c50782f6a8fad49c386da417b850667581506
Radek Holubowski <radekh8787@gmail.com>
2022-01-13 23:49:45 +0000

    Create Interests.js component

---
+++ src/components/Interests.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Interests = () => {
+  return (
+    <section>
+      <h2>Interests</h2>
+      <p>
+        Food expert. Computing. Reader. Internet fanatic. Setting up networks.
+        Video gamer. Travel geek. Designing websites. Coffee fanatic.
+      </p>
+    </section>
+  );
+};
+export default Interests;
