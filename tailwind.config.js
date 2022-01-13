59672462447c1238128b3267198a3bd11cb6f5f6
Radek Holubowski <radekh8787@gmail.com>
2022-01-13 23:20:54 +0000

    Initalize project using create react app and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      width:{
+        card:'550px',
+      },
+      height: {
+        card: '870px',
+      },
+    },
+  },
+  plugins: [],
+};
