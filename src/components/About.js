2a4bf208e3d026f3e51b5152da8a6d588c9741a5
Radek Holubowski <radekh8787@gmail.com>
2022-01-13 23:32:34 +0000

    Create About.js component

---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <h2>About</h2>
+      <p>
+        I am a full stack developer with a particular interest in making things
+        simple and automating daily tasks. I try to keep up with security and
+        best practices, and am always looking for new things to learn.
+      </p>
+    </section>
+  );
+};
+export default About;
