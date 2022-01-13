827b256f3cb8b98c2e521d5cc6465f3aa617e8af
Radek Holubowski <radekh8787@gmail.com>
2022-01-13 23:27:10 +0000

    Create Info component

---
+++ src/components/Info.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Info = () => {
+  return (
+    <header>
+      <img src="" alt="" />
+      <h1>rado holubowski</h1>
+      <h3>full stack developer</h3>
+      <p>radoh.website</p>
+      <div>
+        <button className="btn1">email</button>
+        <button className="btn2">linkedin</button>
+      </div>
+    </header>
+  );
+};
+export default Info;
