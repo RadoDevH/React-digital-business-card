ce4ee0ca82577b6433fa957865fa2530085390e6
Radek Holubowski <radekh8787@gmail.com>
2022-01-14 00:16:31 +0000

    Create Footer.js component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://twitter.com/"></a>
+      <a href="https://www.facebook.com/"></a>
+      <a href="https://www.instagram.com/"></a>
+      <a href=">https://github.com/"></a>
+    </footer>
+  );
+};
+export default Footer;
