9a548515212435b1a2d20cba0b62bb19eeb7789f
Radek Holubowski <radekh8787@gmail.com>
2022-01-15 20:52:07 +0000

    add styling to Interests component

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Interests = () => {
   return (
-    <section>
-      <h2>Interests</h2>
+    <section className="px-12">
+      <h2 className="font-bold py-3">Interests</h2>
       <p>
         Food expert. Computing. Reader. Internet fanatic. Setting up networks.
         Video gamer. Travel geek. Designing websites. Coffee fanatic.
